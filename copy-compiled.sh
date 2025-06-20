afc2bb4a8a70efc5bb358939cd0a282bce59daab
Artiom Levshukov <artiomlevshykov@gmail.com>
2025-06-20 11:46:05 +0300

    Fix script structure

--- copy-compiled.sh
+++ copy-compiled.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 mkdir -p ./precompiled
 find .pio -type f \( -name 'firmware.hex' -o -name 'firmware.elf' \) -exec sh -c 'for f; do d=./precompiled/${f#.pio/build/}; mkdir -p "${d%/*}"; cp -v "$f" "$d"; done' sh {} +
